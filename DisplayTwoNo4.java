4663f08780c3bc3d140a8c1e772fbecba76ce688
AthaHalilintar <Atharasya088@gmail.com>
2025-11-04 08:07:06 +0700

    AnswerForDisplay

---
+++ DisplayTwoNo4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class DisplayTwoNo4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int numInput;
+
+        System.out.print("Input some number: ");
+        numInput = input.nextInt();
+
+        for (int i = 1; i <= numInput; i++) {
+            if (i % 2 == 0) {
+                System.out.println("2 multiple: " + i);
+            }
+        }
+        input.close();
+    }
+}
+
+
+    
+
