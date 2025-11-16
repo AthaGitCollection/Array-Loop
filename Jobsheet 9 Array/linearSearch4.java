86a30e20f64f09ba64502bbd61302bdc278021a5
AthaHalilintar <Atharasya088@gmail.com>
2025-11-16 22:41:53 +0700

    Question No 1,2,3

--- Jobsheet 9 Array/linearSearch4.java
+++ Jobsheet 9 Array/linearSearch4.java
@@ -1,15 +1,41 @@
+import java.util.Scanner;
+
 public class linearSearch4 {
-    public static void main (String[] args) {
-        int[] arrayInt = { 34, 18, 26, 48, 72, 20, 56, 63 };
-        int key = 20;
-        int result = 0;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+       
+        System.out.print("Enter the number of array elements: ");
+        int n = scanner.nextInt();
+        
+        
+        int[] arrayInt = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter the array element " + i + ": ");
+            arrayInt[i] = scanner.nextInt();
+        }
+        
+       
+        System.out.print("Enter the key you want to search for: ");
+        int key = scanner.nextInt();
+        
+       
+        int result = -1; 
+        
         for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {
                 result = i;
-                break;
+                break; 
             }
         }
-        System.out.println("The key in the array is locate at index position" + result);
+        
+        
+        if (result != -1) {
+            System.out.println("The key in the array is located at index position " + result);
+        } else {
+            System.out.println("Key not found");
+        }
+        
+        scanner.close();
     }
-    
 }
