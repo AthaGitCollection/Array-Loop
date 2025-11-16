e014e2babec737baa44dc9e42510b56218bce9db
AthaHalilintar <Atharasya088@gmail.com>
2025-11-16 22:22:42 +0700

    Experiment 3 Number 2

--- Jobsheet 9 Array/ArrayAverageScore4.java
+++ Jobsheet 9 Array/ArrayAverageScore4.java
@@ -4,19 +4,32 @@ public class ArrayAverageScore4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] finalScore = new int[10];
+        System.out.print("Enter the number of students: ");
+        int n = sc.nextInt();
+
+        int[] score = new int[n];
+
+        double passTotal = 0;
+        double failTotal = 0;
         int passCount = 0;
+        int failCount = 0;
 
-        for (int i = 0; i < finalScore.length; i++) {
+        for (int i = 0; i < n; i++) {
             System.out.print("Enter the final score " + i + ": ");
-            finalScore[i] = sc.nextInt();
+            score[i] = sc.nextInt();
 
-            if (finalScore[i] > 70) {
+            if (score[i] > 70) {
+                passTotal += score[i];
                 passCount++;
+            } else {
+                failTotal += score[i];
+                failCount++;
             }
         }
 
-        System.out.println("Number of students who passed (>70): " + passCount);
+        System.out.println("The average score of students who passed is " + (passTotal / passCount));
+        System.out.println("The average score of students who failed is " + (failTotal / failCount));
+
         sc.close();
     }
 }
