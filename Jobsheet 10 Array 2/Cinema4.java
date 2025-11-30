7bba38abf5d5e1ba6119cd5340b00fa9a1ce0943
AthaHalilintar <Atharasya088@gmail.com>
2025-11-30 17:54:09 +0700

    number 12

--- Jobsheet 10 Array 2/Cinema4.java
+++ Jobsheet 10 Array 2/Cinema4.java
@@ -17,10 +17,10 @@ public class Cinema4 {
         System.out.printf("%s \t %s \n", audience[3][0], audience[3][1]);
 
         
-        System.out.println("Audiences in the row 3: ");
-for (String i : audience[2]) {
-    System.out.println(i);
-}
+        for (int i = 0; i < audience.length; i++) {
+    System.out.println("Audience in the row: " + (i + 1) + " " + String.join(", ", audience[i]));
+    }   
 
-    }
-}
+        }
+            }
+
